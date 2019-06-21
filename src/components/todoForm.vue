7133717ae03afa97074f5d05569205705d6081cc
Erika-Agana <agan0003@algonquinlive.com>
2019-06-21 12:51:50 -0400

    added a form and slots in the form component

---
+++ src/components/todoForm.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <form @submit.prevent="addTodo" >
+            <input type="text" v-model="newTodo" >
+        </form>
+        <slot name="desc" > </slot>
+        <br/>
+        <slot> </slot>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            newTodo:" "
+        };
+    },
+    methods: {
+        addTodo() {
+            if (this.newTodo.length) {
+                this.$emit("newTodo", this.newTodo);
+                this.newTodo = "";
+            }
+        }
+    }
+};
+</script>
+
